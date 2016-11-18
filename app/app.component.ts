2f34cefdb946d319fe43da3fda32d86540bb5ca5
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2016-11-18 11:36:29 +0100

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-app',
+    template: "<my-dashboard></my-dashboard>"
+})
+export class AppComponent {
+    title = 'Whatever';
+}
