45aab5e4551a85c70bcf6b1a6721c1f956b1ed47
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2016-12-14 22:12:56 +0100

    Add notification plugin and call first method

---
+++ scripts/notification.ts
@@ -0,0 +1,27 @@
+class Notification {
+    public constructor(
+        public readonly id: number,
+        public readonly title: string,
+        public readonly text: string,
+        public readonly sound: string,
+        public readonly firstAt: number,
+        public readonly at: number,
+        public readonly every: number,
+        public readonly data: any) {
+    }
+}
+
+interface Local {
+    schedule(notifications: Notification | Notification[], callback?: (notification: Notification) => void, scope?: any, args?: any): void;
+    on(event: "trigger", callback: (notification: Notification) => void, scope?: any): void;
+    on(event: "click", callback: (notification: Notification) => void, scope?: any): void;
+    hasPermission(callback: (granted: boolean) => void, scope?: any);
+}
+
+interface Notification {
+    local: Local;
+}
+
+interface CordovaPlugins {
+    notification: Notification;
+}
