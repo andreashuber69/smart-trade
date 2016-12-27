a71787e45b25d560a5e3f1cd07a50b83a034bcbb
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2016-12-27 22:21:45 +0100

    Add Bitstamp project

---
+++ Bitstamp/FakeActivity.cs
@@ -0,0 +1,19 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// <copyright>Copyright 2016-2017 Andreas Huber Dönni.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)</copyright>
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace Bitstamp
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    using Android.App;
+
+    /// <summary>Fake main activity to work around https://bugzilla.xamarin.com/show_bug.cgi?id=43553.</summary>
+    [SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Instantiated through reflection.")]
+    [Activity(MainLauncher = true)]
+    internal sealed class FakeActivity : Activity
+    {
+    }
+}
