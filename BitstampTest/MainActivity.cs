520bc06b159cb3d82fa9bd596531ff6c0bd0281b
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2016-12-29 09:08:24 +0100

    Add BitstampTest project

---
+++ BitstampTest/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿namespace BitstampTest
+{
+    using System.Reflection;
+    using Android.App;
+    using Android.OS;
+    using Xamarin.Android.NUnitLite;
+
+    [Activity(Label = "BitstampTest", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : TestSuiteActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            // tests can be inside the main assembly
+            AddTest(Assembly.GetExecutingAssembly());
+            // or in any reference assemblies
+            // AddTest (typeof (Your.Library.TestClass).Assembly);
+
+            // Once you called base.OnCreate(), you cannot add more assemblies.
+            base.OnCreate(bundle);
+        }
+    }
+}
+
