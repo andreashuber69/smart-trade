2fae2431e895885831e8409c0b446b5d47cae023
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2017-09-22 16:03:40 +0200

    Closes #14

--- SmartTrade/MainActivity.cs
+++ SmartTrade/MainActivity.cs
@@ -35,19 +35,8 @@ namespace SmartTrade
             this.lastTradeBalance2TextView = this.FindViewById<TextView>(Resource.Id.last_trade_balance2_text_view);
             this.nextTradeTimeTextView = this.FindViewById<TextView>(Resource.Id.next_trade_time_text_view);
 
-            this.service.PropertyChanged += this.UpdateView;
-            this.service.Settings.PropertyChanged += this.UpdateView;
-
-            // https://stackoverflow.com/questions/4597690/android-timer-how-to
-            this.updateAction =
-                () =>
-                {
-                    this.UpdateView();
-                    this.updateHandler.PostDelayed(this.updateAction, 10000);
-                };
-
-            this.updateHandler.PostDelayed(this.updateAction, 10000);
-
+            this.service.PropertyChanged += this.OnPropertyChanged;
+            this.service.Settings.PropertyChanged += this.OnPropertyChanged;
             this.UpdateView();
         }
 
@@ -55,9 +44,9 @@ namespace SmartTrade
         {
             if (disposing)
             {
-                this.updateHandler.RemoveCallbacks(this.updateAction);
-                this.service.Settings.PropertyChanged -= this.UpdateView;
-                this.service.PropertyChanged -= this.UpdateView;
+                this.updateHandler.RemoveCallbacks(this.OnTimeElapsed);
+                this.service.Settings.PropertyChanged -= this.OnPropertyChanged;
+                this.service.PropertyChanged -= this.OnPropertyChanged;
                 this.service.Dispose();
             }
 
@@ -94,13 +83,40 @@ namespace SmartTrade
 
             var formatted = Invariant($"{Math.Floor(absoluteAmount)} {unit}");
 
-            if (absoluteAmount > 2.0)
+            if (absoluteAmount >= 2.0)
             {
                 formatted += "s";
             }
 
             return amount > 0.0 ? formatted + " ago" : "in " + formatted;
         }
+
+        private static long? GetUpdateDelay(DateTime? dateTime)
+        {
+            var nullable = DateTime.UtcNow - dateTime;
+
+            if (nullable.HasValue)
+            {
+                var span = nullable.Value;
+                var delayMilliseconds =
+                    GetUpdateDelayIf(span.TotalDays) * 24 * 60 * 60 * 1000 ??
+                    GetUpdateDelayIf(span.TotalHours) * 60 * 60 * 1000 ??
+                    GetUpdateDelayIf(span.TotalMinutes) * 60 * 1000 ??
+                    GetUpdateDelay(span.TotalSeconds) * 1000;
+
+                return (long)delayMilliseconds + 1;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private static double? GetUpdateDelayIf(double amount) =>
+            Abs(amount) > 1.0 ? GetUpdateDelay(amount) : (double?)null;
+
+        private static double GetUpdateDelay(double amount) =>
+            amount > 0.0 ? Ceiling(amount) - amount : amount - Floor(amount);
 
         private readonly BtcEurTradeService service = new BtcEurTradeService();
         private readonly Handler updateHandler = new Handler();
@@ -113,7 +129,6 @@ namespace SmartTrade
         private TextView lastTradeBalance1TextView;
         private TextView lastTradeBalance2TextView;
         private TextView nextTradeTimeTextView;
-        private Action updateAction;
 
         private EditText GetCustomerIdEditText()
         {
@@ -147,24 +162,46 @@ namespace SmartTrade
         private ToggleButton GetEnableDisableServiceButton()
         {
             var result = this.FindViewById<ToggleButton>(Resource.Id.enable_disable_service_button);
-            result.Click += (s, e) => this.service.IsEnabled = result.Checked;
+            result.Click +=
+                (s, e) =>
+                {
+                    this.service.IsEnabled = result.Checked;
+                    this.OnTimeElapsed();
+                };
             return result;
         }
 
-        private void UpdateView(object sender = null, PropertyChangedEventArgs e = null)
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            switch (e?.PropertyName)
+            switch (e.PropertyName)
             {
                 case nameof(ISettings.CustomerId):
                 case nameof(ISettings.ApiKey):
                 case nameof(ISettings.ApiSecret):
                     // These settings are only ever changed from the view itself, the view is therefore already up to
                     // date. Setting the same value again will reset the cursor and thus make the EditText unusable.
-                    return;
+                    break;
                 default:
+                    this.UpdateView();
                     break;
             }
+        }
 
+        private void OnTimeElapsed()
+        {
+            if (this.service.IsEnabled)
+            {
+                var updateDelay = this.UpdateView();
+
+                if (updateDelay.HasValue)
+                {
+                    this.updateHandler.PostDelayed(this.OnTimeElapsed, updateDelay.Value);
+                }
+            }
+        }
+
+        private long? UpdateView()
+        {
             var settings = this.service.Settings;
             this.customerIdEditText.Text =
                 settings.CustomerId == 0 ? string.Empty : settings.CustomerId.ToString(InvariantCulture);
@@ -184,11 +221,25 @@ namespace SmartTrade
             if (settings.NextTradeTime == 0)
             {
                 this.nextTradeTimeTextView.Text = Format(null);
+                return GetUpdateDelay(settings.LastTradeTime);
             }
             else
             {
-                this.nextTradeTimeTextView.Text = Format(DateTime.UtcNow +
-                    TimeSpan.FromMilliseconds(settings.NextTradeTime - Java.Lang.JavaSystem.CurrentTimeMillis()));
+                var nextTradeTime = DateTime.UtcNow +
+                    TimeSpan.FromMilliseconds(settings.NextTradeTime - Java.Lang.JavaSystem.CurrentTimeMillis());
+                this.nextTradeTimeTextView.Text = Format(nextTradeTime);
+
+                var lastDelay = GetUpdateDelay(settings.LastTradeTime);
+                var nextDelay = GetUpdateDelay(nextTradeTime);
+
+                if (lastDelay.HasValue && nextDelay.HasValue)
+                {
+                    return Min(lastDelay.Value, nextDelay.Value);
+                }
+                else
+                {
+                    return lastDelay ?? nextDelay;
+                }
             }
         }
     }
