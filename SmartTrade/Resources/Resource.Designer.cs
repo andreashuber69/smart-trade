72be09d86f5c1893abce37b48bb1ea6f8df63f07
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2017-09-27 07:19:31 +0200

    Closes #19

--- SmartTrade/Resources/Resource.Designer.cs
+++ SmartTrade/Resources/Resource.Designer.cs
@@ -163,11 +163,11 @@ namespace SmartTrade
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f060008
-			public const int ApiKey = 2131099656;
+			// aapt resource value: 0x7f06000c
+			public const int ApiKey = 2131099660;
 			
-			// aapt resource value: 0x7f060009
-			public const int ApiSecret = 2131099657;
+			// aapt resource value: 0x7f06000d
+			public const int ApiSecret = 2131099661;
 			
 			// aapt resource value: 0x7f060001
 			public const int AppDescription = 2131099649;
@@ -175,50 +175,62 @@ namespace SmartTrade
 			// aapt resource value: 0x7f060000
 			public const int AppName = 2131099648;
 			
-			// aapt resource value: 0x7f06000e
-			public const int BoughtPopup = 2131099662;
+			// aapt resource value: 0x7f060007
+			public const int Balance = 2131099655;
 			
-			// aapt resource value: 0x7f06000c
-			public const int CheckingPopup = 2131099660;
+			// aapt resource value: 0x7f060012
+			public const int BoughtPopup = 2131099666;
 			
-			// aapt resource value: 0x7f060011
-			public const int InsufficientBalancePopup = 2131099665;
+			// aapt resource value: 0x7f060010
+			public const int CheckingPopup = 2131099664;
+			
+			// aapt resource value: 0x7f060015
+			public const int InsufficientBalancePopup = 2131099669;
 			
 			// aapt resource value: 0x7f060005
 			public const int LastTrade = 2131099653;
 			
-			// aapt resource value: 0x7f060010
-			public const int NoDepositPopup = 2131099664;
+			// aapt resource value: 0x7f060009
+			public const int NextTrade = 2131099657;
 			
-			// aapt resource value: 0x7f06000d
-			public const int NothingToTradePopup = 2131099661;
+			// aapt resource value: 0x7f060014
+			public const int NoDepositPopup = 2131099668;
+			
+			// aapt resource value: 0x7f060011
+			public const int NothingToTradePopup = 2131099665;
+			
+			// aapt resource value: 0x7f060008
+			public const int Result = 2131099656;
 			
 			// aapt resource value: 0x7f060003
 			public const int ServiceIsDisabled = 2131099651;
 			
-			// aapt resource value: 0x7f06000a
-			public const int ServiceIsDisabledPopup = 2131099658;
+			// aapt resource value: 0x7f06000e
+			public const int ServiceIsDisabledPopup = 2131099662;
 			
 			// aapt resource value: 0x7f060004
 			public const int ServiceIsEnabled = 2131099652;
 			
-			// aapt resource value: 0x7f06000b
-			public const int ServiceIsEnabledPopup = 2131099659;
+			// aapt resource value: 0x7f06000f
+			public const int ServiceIsEnabledPopup = 2131099663;
 			
 			// aapt resource value: 0x7f060002
 			public const int Settings = 2131099650;
 			
+			// aapt resource value: 0x7f06000a
+			public const int SettingsTitle = 2131099658;
+			
+			// aapt resource value: 0x7f060013
+			public const int SoldPopup = 2131099667;
+			
 			// aapt resource value: 0x7f060006
-			public const int SettingsTitle = 2131099654;
+			public const int Time = 2131099654;
 			
-			// aapt resource value: 0x7f06000f
-			public const int SoldPopup = 2131099663;
+			// aapt resource value: 0x7f060016
+			public const int UnexpectedErrorPopup = 2131099670;
 			
-			// aapt resource value: 0x7f060012
-			public const int UnexpectedErrorPopup = 2131099666;
-			
-			// aapt resource value: 0x7f060007
-			public const int UserId = 2131099655;
+			// aapt resource value: 0x7f06000b
+			public const int UserId = 2131099659;
 			
 			static String()
 			{
