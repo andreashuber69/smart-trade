2448fbce30214921981453d227bcd1bc31e8396b
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2017-10-07 20:09:19 +0200

    Add logging for unhandled exceptions
    References #29

--- SmartTrade/MainActivity.cs
+++ SmartTrade/MainActivity.cs
@@ -6,7 +6,9 @@
 
 namespace SmartTrade
 {
+    using System;
     using System.Linq;
+    using System.Threading.Tasks;
 
     using Android.App;
     using Android.Content;
@@ -15,6 +17,9 @@ namespace SmartTrade
     using Android.Views;
     using Android.Widget;
     using Bitstamp;
+
+    using static Logger;
+    using static System.Environment;
 
     [Activity(Label = "@string/AppName", MainLauncher = true, Icon = "@mipmap/icon", ScreenOrientation = ScreenOrientation.Portrait)]
     internal sealed class MainActivity : ActivityBase, AdapterView.IOnItemClickListener
@@ -30,6 +35,13 @@ namespace SmartTrade
         protected sealed override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
+            AppDomain.CurrentDomain.UnhandledException +=
+                (s, e) => Error("Unhandled Exception!{0}{1}", NewLine, e.ExceptionObject);
+
+            TaskScheduler.UnobservedTaskException +=
+                (s, e) => Error("Unobserved Task Exception!{0}{1}", NewLine, e.Exception);
+
             this.SetContentView(Resource.Layout.Main);
 
             this.tickersListView = this.FindViewById<ListView>(Resource.Id.TickersListView);
